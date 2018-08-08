3f54b169dcc7db2dfd86a8a9a4101643a62064c3
airbendor890 <42207312+airbendor890@users.noreply.github.com>
2018-08-08 11:42:47 +0000

    Add files via upload

---
+++ ddd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{ // my first proggramm in niser
+  cout<<"hello 0101010101\n";
+  cout<<"best of luck for next 5 years"<<endl;
+  
+  return 0;  
+
+
+} 
