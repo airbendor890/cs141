0180cd3d3eb9769af75877c59602df41177971c6
airbendor890 <42207312+airbendor890@users.noreply.github.com>
2018-09-02 23:56:27 +0530

    Add files via upload

---
+++ lab5.6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{	//prgm to fnd sum of frst n naturla numbers
+	cout<<"//prgm to fnd sum of frst n naturla numbers"<<endl;	
+	int sum=0,n;
+	cout<<"input 'n'"<<endl;
+	cin>>n;
+	for(int i=0;i<=n;i++)  //fr lp
+	   {sum=sum+i;		//sum acts as a chngn var which keeps on addng itslf to fnd sum
+		cout<<".";
+	    
+           }
+	cout<<"\nsum==="<<sum<<endl;
+
+	return 0;
+
+
+
+
+
+}
